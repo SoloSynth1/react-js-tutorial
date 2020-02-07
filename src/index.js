17fa459e8359ebdbf8095b4eb1c4b63769c2a8f5
OrixAY <orix.auyeung@hunt.social>
2020-02-07 20:39:55 +0800

    add answer for optional task #6

--- src/index.js
+++ src/index.js
@@ -79,7 +79,9 @@ class Game extends React.Component {
         if (calculateWinner(squares).winner || squares[i]) {
             return;
         }
+
         squares[i] = this.state.xIsNext ? 'X' : 'O';
+
         this.setState({
             history: history.concat([{
                 squares: squares,
@@ -87,7 +89,7 @@ class Game extends React.Component {
                 col: col,
             }]),
             stepNumber: history.length,
-            xIsNext: !this.state.xIsNext
+            xIsNext: !this.state.xIsNext,
         });
     }
 
@@ -116,7 +118,6 @@ class Game extends React.Component {
         const winInfo = calculateWinner(current.squares);
         const winner = winInfo.winner;
         const movesIsAsc = this.state.movesIsAsc;
-
         const moves = history.map((step, move) => {
             const col = this.state.history[move].col;
             const row = this.state.history[move].row;
@@ -142,6 +143,8 @@ class Game extends React.Component {
         let status;
         if (winner) {
             status = 'Winner: ' + winner;
+        } else if (winInfo.isDraw) {
+            status = 'Draw!'
         } else {
             status = 'Next player: ' + (this.state.xIsNext? "X": "O");
         }
@@ -182,17 +185,29 @@ function calculateWinner(squares) {
         [0, 4, 8],
         [2, 4, 6],
     ];
+
+
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
             return {
                 winner: squares[a],
                 line: lines[i],
+                isDraw: false,
             };
+        }
+    }
+
+    let isDraw = true;
+    for (let i = 0; i < squares.length; i++) {
+        if (squares[i] === null) {
+            isDraw = false;
+            break;
         }
     }
     return {
         winner: null,
         line: null,
+        isDraw: isDraw,
     };
 }
